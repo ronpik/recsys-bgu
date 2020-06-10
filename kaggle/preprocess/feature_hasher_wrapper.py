937ba85ffc65fb1594b9a3297a1d648fbca3aa3f
ronpik <ronpi1310@gmail.com>
2020-06-10 13:21:52 +0300

    create feature hashing

---
+++ kaggle/preprocess/feature_hasher_wrapper.py
@@ -0,0 +1,32 @@
+from typing import Sequence, Tuple, Dict, List
+
+import numpy as np
+import scipy.sparse
+from sklearn.feature_extraction import FeatureHasher
+
+from kaggle.preprocess.utils import filter_by_occurrence, create_categories_index_mapping
+
+
+class FeatureFilterHasher(object):
+    def __init__(self, feature_prefix: str, min_occurrence_threshold: int = 0, **hasher_kwargs):
+        self.feature_prefix = feature_prefix
+        self.min_occurrence_threshold = min_occurrence_threshold
+        self.hasher_kwargs = hasher_kwargs
+        self.hasher_kwargs["input_type"] = "string"
+
+        # post fit members
+        self.num_features: int = None
+        self.hasher: FeatureHasher = None
+        self.features_names: Sequence[str] = None
+
+    def fit(self, values: Sequence[str]) -> 'FeatureFilterHasher':
+        if (self.min_occurrence_threshold > 0):
+            values = filter_by_occurrence(values, self.min_occurrence_threshold)
+
+        self.num_features = np.log2(len(values))
+        self.hasher = FeatureHasher(n_features=self.num_features, **self.hasher_kwargs)
+        self.features_names = [f"{self.feature_prefix}:{i}" for i in range(self.num_features)]
+        return self
+
+    def transform(self, values: Sequence[str]) -> scipy.sparse:
+        return self.hasher.transform(values)
---
+++ kaggle/preprocess/features_hashing.py
@@ -0,0 +1,163 @@
+from itertools import chain
+from operator import itemgetter
+from typing import Tuple, Sequence, Set, Any, Dict, List
+
+import numpy as np
+import pandas as pd
+import scipy.sparse
+from scipy.sparse import csr_matrix, hstack as sparse_hstack
+
+from kaggle.preprocess.feature_hasher_wrapper import FeatureFilterHasher
+from kaggle.preprocess.numeric import get_numeric_headers, process_numeric_features
+from kaggle.preprocess.temporal import VIEW_TIME_FIELD, get_temporal_headers, process_temporal_features
+from kaggle.preprocess.utils import filter_by_occurrence, create_categories_index_mapping, \
+    NON_FREQ_NAME
+
+
+# todo replace dict by list of tuples to maintain order in all python versions
+FEATURES_MIN_THRESHOLDS = [
+    ("target_id_hash", 5),
+    ("campaign_id_hash", 5),
+    ("syndicator_id_hash", 5),
+    ("placement_id_hash", 5),
+    ("source_id_hash", 5),
+    ("country_code", 5),
+    ("region", 5)
+]
+
+TEMPORAL_FEATURES = [
+    "page_view_start_time",
+    "day_of_week",
+    "time_of_day",
+    "gmt_offset"
+]
+
+OTHER_CATEGORICAL_FEATURES = [
+    "publisher_id_hash",
+    "source_item_type",
+    "browser_platform",
+    "os_family",
+]
+
+TARGET_TAXONOMY_FIELD = "target_item_taxonomy"
+NO_TAX_CATEGORY_VALUE = "NO_CATEGORY"
+
+LABEL = "is_click"
+
+
+class FeaturesHashingProcessor(object):
+    def __init__(self):
+        # map between field and category name to the index in the one hot encoding og this feature
+        self.page_view_min_time: int = None
+        self.features_names: List[str] = None
+        self.__taxonomy_max_depth: int = None
+        self.hashers: Dict[str, FeatureFilterHasher] = None
+
+    def fit(self, data: pd.DataFrame) -> 'FeaturesHashingProcessor':
+        """
+
+        :param data:
+        :return:
+        """
+        self.hashers = {}
+        headers = []
+        for field, threshold in FEATURES_MIN_THRESHOLDS:
+            hasher = FeatureFilterHasher(feature_prefix=field, min_occurrence_threshold=threshold)
+            self.hashers[field] = hasher.fit(data[field])
+            headers.append(hasher.features_names)
+
+        for field in OTHER_CATEGORICAL_FEATURES:
+            assert(field not in FEATURES_MIN_THRESHOLDS)    # just in case, to prevent duplicated huge one hot feature!
+            hasher = FeatureFilterHasher(feature_prefix=field)
+            self.hashers[field] = hasher.fit(data[field])
+            headers.append(hasher.features_names)
+
+        tax_hashers, tax_features_names = create_taxonomy_hashers(data[TARGET_TAXONOMY_FIELD])
+        headers.append(tax_features_names)
+        self.__taxonomy_max_depth = len(tax_hashers)
+        for i in range(len(tax_hashers)):
+            mapping_name = f"tax{i}"
+            self.hashers[mapping_name] = tax_hashers[i]
+
+        numeric_headers = get_numeric_headers()
+        headers.append(numeric_headers)
+
+        self.page_view_min_time = min(data[VIEW_TIME_FIELD])
+        temporal_headers = get_temporal_headers()
+        headers.append(temporal_headers)
+
+        self.features_names = list(chain(*headers))
+        return self
+
+    def transform(self, data: pd.DataFrame) -> csr_matrix:
+        """
+
+        :param data:
+        :return:
+        """
+        features: List[scipy.sparse.spmatrix] = []
+        categorical_features = list(map(itemgetter(0), FEATURES_MIN_THRESHOLDS)) + OTHER_CATEGORICAL_FEATURES
+        for field in categorical_features:
+            categories = np.asarray(data[field]).reshape(-1, 1)
+            values = self.hashers[field].transform(categories)
+            features.append(values)
+
+        # taxonomy features
+        hashers_by_depth = [self.hashers[f"tax{i}"] for i in range(self.__taxonomy_max_depth)]
+        values = create_taxonomy_features(data[TARGET_TAXONOMY_FIELD], hashers_by_depth)
+        features.append(values)
+
+        numeric_features = process_numeric_features(data)
+        features.append(numeric_features)
+
+        temporal_features = process_temporal_features(data, self.page_view_min_time)
+        features.append(temporal_features)
+
+        return sparse_hstack(features, format="csr")
+
+    def fit_transform(self, data: pd.DataFrame) -> csr_matrix:
+        return self.fit(data)\
+            .transform(data)
+
+
+def get_categories_by_depth(taxonomy_values: Sequence[str]) -> List[List[str]]:
+    """
+    extract the taxonomy strings into smaller categories with hierarchy.
+    :param taxonomy_values: list of taxonomy values, where each value contains the ancestors categories, ordered and splitted by '~'.
+    :return: list of lists of categories. the category list at index i, corresponds to the categories in depth i in the taxonomy.
+    """
+    categories_by_hierarchy = [[], [], []]
+    for tax in taxonomy_values:
+        tax_categories = tax.split("~")
+        for i in range(len(categories_by_hierarchy)):
+            category = tax_categories[i] if i < len(tax_categories) else NO_TAX_CATEGORY_VALUE
+            categories_by_hierarchy[i].append(category)
+
+    return categories_by_hierarchy
+
+
+def create_taxonomy_hashers(taxonomy_values: Sequence[str]) -> Tuple[List[FeatureFilterHasher], Sequence[str]]:
+    categories_by_hierarchy = get_categories_by_depth(taxonomy_values)
+
+    hashers = []
+    taxonomy_features_names = []
+    for i in range(len(categories_by_hierarchy)):
+        values = categories_by_hierarchy[i]
+        prefix = f"tax{i}"
+        hasher = FeatureFilterHasher(feature_prefix=prefix)
+        hashers.append(hasher.fit(values))
+        taxonomy_features_names.extend(hasher.features_names)
+
+    return hashers, taxonomy_features_names
+
+
+def create_taxonomy_features(taxonomy_values: Sequence[str], hashers_by_depth: List[FeatureFilterHasher]) -> csr_matrix:
+    categories_by_hierarchy = get_categories_by_depth(taxonomy_values)
+    features_sets = []
+    for i in range(len(categories_by_hierarchy)):
+        categories = np.asarray(categories_by_hierarchy[i]).reshape(-1, 1)
+        values = hashers_by_depth[i].transform(categories)
+        features_sets.append(values)
+
+    tax_features = sparse_hstack(features_sets, format="csr")
+    return tax_features
